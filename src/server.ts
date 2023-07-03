c22ed7dd1d50603417405e16a6287246d74297e4
Lucas Araujo <lucasnsaraujo0310@gmail.com>
2023-07-02 22:32:39 -0300

    configuring typeorm and postgres database using docker-compose

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import swagger from 'swagger-ui-express';
 import swaggerConfig from './swagger.json';
 import { router } from './routes';
+import './database'
 
 const app = express();
 
